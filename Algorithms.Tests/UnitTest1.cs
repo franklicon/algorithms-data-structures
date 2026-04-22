0103d41b9d2602a80eda8b576623ffa1fecf17fa
Francisco Arturo Licon Colon <franciscoliconcolon@gmail.com>
2026-04-22 00:12:13 -0600

    Initial solution structure for algorithms and data structures repo

---
+++ Algorithms.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace Algorithms.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ DataStructures.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace DataStructures.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
