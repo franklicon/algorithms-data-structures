d55810b47e3ea003f262ba85602c402516e94bbc
Francisco Arturo Licon Colon <franciscoliconcolon@gmail.com>
2026-05-13 14:37:03 -0600

    Implement `MergeSort` algorithm with unit tests

--- src/Algorithms/Sorting/SortingAlgorithms.cs
+++ src/Algorithms/Sorting/SortingAlgorithms.cs
@@ -35,4 +35,65 @@ public static class SortingAlgorithms
             }
         }
     }
+
+    public static void MergeSort<T>(T[] array) where T : IComparable<T>
+    {
+        MergeSortAux(array, 0, array.Length - 1);
+    }
+
+    private static void MergeSortAux<T>(T[] array, int left, int right) where T : IComparable<T>
+    {
+        if (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            MergeSortAux<T>(array, left, mid);
+            MergeSortAux<T>(array, mid + 1, right);
+            Merge(array, left, mid, right);
+        }
+    }
+
+    private static void Merge<T>(T[] array, int left, int mid, int right) where T : IComparable<T>
+    {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        T[] leftArray = new T[n1];
+        T[] rightArray = new T[n2];
+        
+        Array.Copy(array, left, leftArray, 0, n1);
+        Array.Copy(array, mid + 1, rightArray, 0, n2);
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2)
+        {
+            if (leftArray[i].CompareTo(rightArray[j]) <= 0)
+            {
+                array[k] = leftArray[i];
+                i++;
+            }
+            else
+            {
+                array[k] = rightArray[j];
+                j++;
+            }
+
+            k++;
+        }
+        
+        while (i < n1)
+        {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        
+        while (j < n2)
+        {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
 }
